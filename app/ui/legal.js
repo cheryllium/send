ae8901be0f47b3578c3115ea259695b55bc43b77
Jason Carr <jcarr250@gmail.com>
2021-04-17 17:15:47 -0400

    Replace mozilla legal link with express lack of any guarantees whatsoever

---
+++ app/ui/legal.js
@@ -0,0 +1,26 @@
+const html = require('choo/html');
+const modal = require('./modal');
+
+module.exports = function(state, emit) {
+  state.modal = null;
+  return html`
+    <main class="main">
+      ${state.modal && modal(state, emit)}
+      <div
+        class="flex flex-col items-center bg-white m-4 px-6 py-8 border border-grey-30 md:border-none md:px-12 md:py-16 shadow w-full md:h-full dark:bg-grey-90"
+      >
+        <h1 class="text-center text-3xl font-bold">
+          Legal
+        </h1>
+        <p class="mt-2">Version 1.0, dated Apr 17, 2021</p>
+        <div class="overflow-y-scroll py-8 px-12">
+          <p class="leading-normal">
+            <p>
+              EXCEPT AS EXPRESSLY PROVIDED IN THIS AGREEMENT, NO PARTY MAKES ANY GUARANTEES OR WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, WHETHER ARISING BY OPERATION OF LAW OR OTHERWISE. PROVIDER SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF MERCHANTABILITY AND/OR ANY IMPLIED WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.
+            </p>
+          </p>
+        </div>
+      </div>
+    </main>
+  `;
+};
