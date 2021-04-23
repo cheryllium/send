f5c2a9b1531ec04e91cae3142ef5d963c1df98f3
C. Yang <hello@cheryllium.com>
2021-04-22 22:26:44 -0400

    ACME challenge static file

--- server/bin/prod.js
+++ server/bin/prod.js
@@ -32,6 +32,13 @@ app.use(
 
 app.use(pages.notfound);
 
+// ACME challenge
+app.use(
+    express.static(
+        path.resolve(__dirname, '../../static/')
+    )
+)
+
 try {
     let options = {
         key: fs.readFileSync(config.ssl_key, 'utf8'),
